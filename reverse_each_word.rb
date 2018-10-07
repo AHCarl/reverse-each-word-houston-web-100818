a72f2944b04f0596a3f5fef19a656c91301ba7ea
Alexander Carlin <ahcarlin@gmail.com>
2018-10-07 04:32:26 +0000

    automatically backed up by learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,10 @@
+def reverse_each_word(sentence)
+  reversed = []
+  reversed = sentence.split(" ")
+  output=""
+  
+  reversed.each do |word|
+    output << word.to_s.reverse + " " 
+  end
+  output[output.rindex(" ")]
+end
